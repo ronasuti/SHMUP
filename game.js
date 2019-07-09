62f6d61a8eb569b07e0a31c50bf562b9ef3c3099
AbenewNasuti <nasuti_roman@abenew.co.jp>
2019-07-09 13:39:18 +0900

    pull in pro.mhtml code into index.html and game.js

--- game.js
+++ game.js
@@ -1,47 +1,140 @@
 var config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: 600,
+    height: 700,
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 200 }
+            gravity: { y: 0 },
+            debug: false
         }
     },
     scene: {
         preload: preload,
-        create: create
+        create: create,
+        update: update
     }
 };
 
+var player;
+var platforms;
+var cursors;
+var KeyShift;
 var game = new Phaser.Game(config);
+var bullets;
 
 function preload ()
 {
-    this.load.setBaseURL('http://labs.phaser.io');
-
-    this.load.image('sky', 'assets/skies/space3.png');
-    this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-    this.load.image('red', 'assets/particles/red.png');
+    this.load.image('bg', 'assets/bg.png');
+    this.load.image('ground', 'assets/platform.png');
+    this.load.image('tate', 'assets/tate.png');
+    this.load.spritesheet('jiki', 'assets/jiki.png', { frameWidth: 32, frameHeight: 48 });
+    this.load.image('bullet', 'assets/bullet.png'
 }
 
 function create ()
 {
-    this.add.image(400, 300, 'sky');
+    this.add.image(295,350, 'bg');
 
-    var particles = this.add.particles('red');
+    platforms = this.physics.add.staticGroup();
+    player = this.physics.add.sprite(100, 450, 'jiki');
+   platforms.create(400, 731, 'ground').setScale(2).refreshBody();
+   platforms.create(0.0001, 300, 'tate').setScale(2).refreshBody();
+   platforms.create(615, 300, 'tate').setScale(2).refreshBody();
+    player.setBounce(0.2);
+    player.setDrag(0.99);
+    var Bullet = new Phaser.Class({
 
-    var emitter = particles.createEmitter({
-        speed: 100,
-        scale: { start: 1, end: 0 },
-        blendMode: 'ADD'
+    Extends: Phaser.GameObjects.Image,
+
+    initialize:
+
+    function Bullet (scene)
+    {
+        Phaser.GameObjects.Image.call(this, scene, 0, 0, 'bullet');
+
+        this.speed = Phaser.Math.GetSpeed(400, 1);
+    },
+
+    fire: function (x, y)
+    {
+        this.setPosition(x, y - 50);
+
+        this.setActive(true);
+        this.setVisible(true);
+    },
+
+    update: function (time, delta)
+    {
+        this.y -= this.speed * delta;
+
+        if (this.y < -50)
+        {
+            this.setActive(false);
+            this.setVisible(false);
+        }
+    }
+
+    bullets = this.add.group({
+           classType: Bullet,
+           maxSize: 10,
+           runChildUpdate: true
+       });
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('jiki', { start: 1, end: 1 }),
+        frameRate: 10,
+        repeat: -1
     });
 
-    var logo = this.physics.add.image(400, 100, 'logo');
+    this.anims.create({
+        key: 'turn',
+        frames: [ { key: 'jiki', frame: 0 } ],
+        frameRate: 20
+    });
 
-    logo.setVelocity(100, 200);
-    logo.setBounce(1, 1);
-    logo.setCollideWorldBounds(true);
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('jiki', { start: 1, end: 1 }),
+        frameRate: 10,
+        repeat: -1
+    });
 
-    emitter.startFollow(logo);
+    cursors = this.input.keyboard.createCursorKeys();
+
+    this.physics.add.collider(player,platforms);
+}
+
+function update ()
+{
+    if (cursors.left.isDown)
+    {
+        player.setVelocityX(-160);
+
+
+    }
+    else if (cursors.right.isDown)
+    {
+        player.setVelocityX(160);
+
+
+    }
+    else
+    {
+        player.setVelocityX(0);
+
+
+    }
+
+    if (cursors.up.isDown)
+    {
+        player.setVelocityY(-160);
+    }
+    else if (cursors.down.isDown)
+    {
+        player.setVelocityY(160);
+    }
+    else{
+      player.setVelocityY(0);
+    }
 }

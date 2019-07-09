37442a6cab940056a4aabb8ae91e79a347413386
AbenewNasuti <nasuti_roman@abenew.co.jp>
2019-07-09 14:31:31 +0900

    fix game.js code formatting so it loads correctly

--- game.js
+++ game.js
@@ -29,7 +29,7 @@ function preload ()
     this.load.image('ground', 'assets/platform.png');
     this.load.image('tate', 'assets/tate.png');
     this.load.spritesheet('jiki', 'assets/jiki.png', { frameWidth: 32, frameHeight: 48 });
-    this.load.image('bullet', 'assets/bullet.png'
+    this.load.image('bullet', 'assets/bullet.png');
 }
 
 function create ()
@@ -38,70 +38,63 @@ function create ()
 
     platforms = this.physics.add.staticGroup();
     player = this.physics.add.sprite(100, 450, 'jiki');
-   platforms.create(400, 731, 'ground').setScale(2).refreshBody();
-   platforms.create(0.0001, 300, 'tate').setScale(2).refreshBody();
-   platforms.create(615, 300, 'tate').setScale(2).refreshBody();
+    platforms.create(400, 731, 'ground').setScale(2).refreshBody();
+    platforms.create(0.0001, 300, 'tate').setScale(2).refreshBody();
+    platforms.create(615, 300, 'tate').setScale(2).refreshBody();
     player.setBounce(0.2);
     player.setDrag(0.99);
     var Bullet = new Phaser.Class({
-
-    Extends: Phaser.GameObjects.Image,
-
-    initialize:
-
-    function Bullet (scene)
-    {
-        Phaser.GameObjects.Image.call(this, scene, 0, 0, 'bullet');
-
-        this.speed = Phaser.Math.GetSpeed(400, 1);
-    },
-
-    fire: function (x, y)
-    {
-        this.setPosition(x, y - 50);
-
-        this.setActive(true);
-        this.setVisible(true);
-    },
-
-    update: function (time, delta)
-    {
-        this.y -= this.speed * delta;
-
-        if (this.y < -50)
+        Extends: Phaser.GameObjects.Image,
+        initialize:
+        function Bullet (scene)
         {
-            this.setActive(false);
-            this.setVisible(false);
-        }
-    }
+            Phaser.GameObjects.Image.call(this, scene, 0, 0, 'bullet');
 
+            this.speed = Phaser.Math.GetSpeed(400, 1);
+        },
+
+        fire: function (x, y)
+        {
+            this.setPosition(x, y - 50);
+
+            this.setActive(true);
+            this.setVisible(true);
+        },
+
+        update: function (time, delta)
+        {
+            this.y -= this.speed * delta;
+
+            if (this.y < -50)
+            {
+                this.setActive(false);
+                this.setVisible(false);
+            }
+        }
+    });
     bullets = this.add.group({
-           classType: Bullet,
-           maxSize: 10,
-           runChildUpdate: true
-       });
+        classType: Bullet,
+        maxSize: 10,
+        runChildUpdate: true
+    });
     this.anims.create({
         key: 'left',
         frames: this.anims.generateFrameNumbers('jiki', { start: 1, end: 1 }),
         frameRate: 10,
         repeat: -1
     });
-
-    this.anims.create({
+   this.anims.create({
         key: 'turn',
         frames: [ { key: 'jiki', frame: 0 } ],
         frameRate: 20
     });
-
     this.anims.create({
         key: 'right',
         frames: this.anims.generateFrameNumbers('jiki', { start: 1, end: 1 }),
         frameRate: 10,
         repeat: -1
     });
-
     cursors = this.input.keyboard.createCursorKeys();
-
     this.physics.add.collider(player,platforms);
 }
 
